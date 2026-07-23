87f07ae3f547f88bb004d4527d9b6578eb4ffc9e
reggycarl74 <reggycarl74@yahoo.com>
2026-07-23 07:17:45 +0200

    Prevent demo seed audit timeout

--- app/models/concerns/auditable_changes.rb
+++ app/models/concerns/auditable_changes.rb
@@ -2,12 +2,16 @@ module AuditableChanges
   extend ActiveSupport::Concern
 
   included do
-    after_update_commit :record_audit_event
+    after_update_commit :record_audit_event, unless: :audit_events_suppressed?
   end
 
   private
 
   def record_audit_event
     AuditEvent.create!(school: audit_school, user: Current.user, auditable: self, action: "updated", changes_data: saved_changes)
+  end
+
+  def audit_events_suppressed?
+    ActiveModel::Type::Boolean.new.cast(ENV["DISABLE_AUDIT_EVENTS"])
   end
 end
